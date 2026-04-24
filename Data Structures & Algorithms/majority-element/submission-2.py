f702483edb2ab8a0003a8ad85c19314c50189c24
Dev-Salem <108498811+Dev-Salem@users.noreply.github.com>
2026-04-25 00:33:44 +0300

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # res = {}
+        # for i in nums:
+        #     res[i] =res.get(i, -1) + 1
+        # print(f' the res is {res}')
+        # sortedItems = sorted(res.items(), key=lambda x: x[1], reverse=True)
+        # print(f'sorted items {sortedItems}')
+        # return sortedItems[0][0]
+        counts = {}
+        for i in nums:
+            counts[i] = nums.count(i)
+        print(f' the count is {counts}')
+        maxKey = max(counts, key=counts.get)
+        print(f'the max is {maxKey}')
+        return maxKey
+
